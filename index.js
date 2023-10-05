26f5ed8cfd7b3fa7af8cf9d351260d70088c2d0e
Richard Gahér <richard.gaher@powerplay.studio>
2023-10-05 21:06:12 +0200

    updated sockets

--- index.js
+++ index.js
@@ -18,77 +18,154 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
-const NodeCache = require("node-cache");
-const twchatCache = new NodeCache({stdTTL: 43200, checkperiod: 43260});
 
-const gerUserNameByUserToken = async (userToken) => {
+const MESSAGE_EXPIRY_TIME_MS = 30000;
+const TRUNCATE_TABLE_TIME_MS = 3000000;
+
+// Function to remove expired messages from recentMessages set
+function cleanupRecentMessages() {
+    recentMessages.forEach((message) => {
+        if (Date.now() - message.messageTime > MESSAGE_EXPIRY_TIME_MS) {
+            recentMessages.delete(message);
+        }
+    });
+}
+
+async function truncateTable() {
+    try {
+        const connection = await pool.getConnection();
+        await connection.execute(`TRUNCATE TABLE chat`);
+        console.log("Truncated table chat");
+        connection.release();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const recentMessages = new Set();
+
+// Set up a timer to clean up expired messages periodically
+setInterval(cleanupRecentMessages, MESSAGE_EXPIRY_TIME_MS);
+setInterval(truncateTable, TRUNCATE_TABLE_TIME_MS);
+
+const getMessagesForWorldAndTown = async (world, town) => {
     try {
         const connection = await pool.getConnection();
 
         // Execute the SQL query
         const [rows] = await connection.execute(
-            'SELECT u.username ' +
-            'FROM users u ' +
-            'INNER JOIN users_session us ON u.unique_id = us.user_uniqueid ' +
-            'WHERE us.user_token = ?',
-            [userToken]
+            'SELECT sender, message, created_at ' +
+            'FROM chat ' +
+            'WHERE world = ? AND town = ? ORDER BY created_at ASC',
+            [world, town]
         );
         connection.release();
 
-        if (rows.length === 0) {
-            return null;
-        }
-
-        return rows[0].username;
+        return {"messages":rows, "length": rows.length};
     } catch (error) {
         return null;
     }
 }
+const saveMessageToDb = async (msg, world, town, player_name) => {
+    try {
+        const connection = await pool.getConnection();
+        await connection.execute(`INSERT INTO chat (sender, message, world, town) VALUES (?, ?, ?, ?)`, [player_name, msg, world, town]);
+        connection.release();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+let playerToWorldTown = {};
+
+// Function to associate a player with a world and town
+function connectPlayerToWorldTown(playerName, worldName, town) {
+    playerToWorldTown[playerName] = { world: worldName, town: town };
+}
+
+// Function to retrieve the world and town associated with a player
+function getWorldTownForPlayer(playerName) {
+    return playerToWorldTown[playerName];
+}
+
+// Function to get all players in a specific world and town
+function getPlayersInWorldAndTown(worldName, town) {
+    let playersInWorldAndTown = [];
+    for (let playerName in playerToWorldTown) {
+        if (
+            playerToWorldTown.hasOwnProperty(playerName) &&
+            playerToWorldTown[playerName].world === worldName &&
+            playerToWorldTown[playerName].town === town
+        ) {
+            playersInWorldAndTown.push(playerName);
+        }
+    }
+    return playersInWorldAndTown;
+}
+
+function deletePlayerFromWorldAndTown(playerName, worldName, town) {
+    for (let key in playerToWorldTown) {
+        if (playerToWorldTown.hasOwnProperty(key)) {
+            let player = playerToWorldTown[key];
+            if (key === playerName && player.world === worldName && player.town === town) {
+                delete playerToWorldTown[key];
+                console.log("Player", playerName, "in World", worldName, "Town", town, "deleted.");
+                break;
+            }
+        }
+    }
+}
+
+//	"id": "TellReceived" -> ked niekto whisperne
+// v skripte umoznit vytvorit roomku podla mena, poslat notifikaciu ked ti niekto whisperne v hre na mobil
+
 
 io.on('connection', async (socket) => {
     console.log("connected");
-    const gameWorld = socket.handshake.query.gameWorld;
-    console.log(twchatCache.get("" + gameWorld + ""));
-    let mykeys = twchatCache.keys();
-
-    console.log( mykeys );
-    let worldMessages = {};
-    if (twchatCache.get("" + gameWorld + "") !== undefined) {
-        worldMessages = twchatCache.get("" + gameWorld + "");
+    const loginHash = socket.handshake.query.loginHash;
+    console.log(loginHash);
+    let parsed;
+    let isVerified = false;
+    let gameWorld = null;
+    let player_name = null;
+    let town = null;
+    try{
+        parsed = loginHash.split(":");
+        isVerified = parsed[3] !== undefined;
+        gameWorld = parsed[0] ?? null;
+        player_name = parsed[1] ?? null;
+        town = parsed[2] ?? null;
+    } catch (e) {
+        console.log(e);
+        io.to(socket.id).emit("badCredentials");
+    }
+    let clientRoom;
+    if(gameWorld == null || player_name == null || town == null) {
+        io.to(socket.id).emit("badCredentials");
+    } else {
+        clientRoom = gameWorld+"_"+town;
+        socket.join(clientRoom);
+        io.sockets.in(clientRoom).emit('connectToRoom', `${player_name}${isVerified ? ` [✔]` : ``} has joined the room. [World: ${gameWorld}, Town: ${town}]`);
+        connectPlayerToWorldTown(`${player_name}${isVerified ? ` [✔]` : ``}`.toString(), gameWorld, town);
+        io.sockets.in(clientRoom).emit('connectRoomConnectedUsers', getPlayersInWorldAndTown(gameWorld, town));
+        let worldMessages = await getMessagesForWorldAndTown(gameWorld, town);
         io.to(socket.id).emit("twchatCachedMessages", worldMessages);
     }
-    // const userToken = socket.handshake.query.user_token;
-    // if (!userToken) {
-    //     socket.disconnect(true);
-    //     return;
-    // }
-    // let userName = await gerUserNameByUserToken(userToken);
-    //
-    // if (userName === null) {
-    //     socket.disconnect(true);
-    //     return;
-    // }
-    //
-    // console.log(userName + ' a user connected');
+
 
     socket.on('disconnect', () => {
+        deletePlayerFromWorldAndTown(`${player_name}${isVerified ? ` [✔]` : ``}`.toString(), gameWorld, town);
+        io.sockets.in(clientRoom).emit('connectRoomDisconnectedUsers', JSON.stringify({"player_name":`${player_name}${isVerified ? ` [✔]` : ``}`.toString(), "players":getPlayersInWorldAndTown(gameWorld, town)}));
         console.log('user disconnected');
     });
 
-    // socket.on('chat message', (msg) => {
-    //     io.emit('chat message', JSON.stringify({"message":msg, "userName":userName}));
-    // });
-
-    socket.on('twChat', (msg) => {
-        if (twchatCache.get("" + gameWorld + "") !== undefined) {
-            let cachedMessages = twchatCache.get("" + gameWorld + "");
-            let parsed = [JSON.parse(cachedMessages)];
-            parsed = parsed.push(JSON.parse(msg));
-            twchatCache.set("" + gameWorld + "", JSON.stringify(parsed));
-        } else {
-            twchatCache.set("" + gameWorld + "", msg);
+    socket.on('twChat', async (msg) => {
+        if (!recentMessages.has(msg)){
+            recentMessages.add(msg);
+            msg = JSON.parse(msg);
+            await saveMessageToDb(msg.message, gameWorld, town, msg.sender);
+            io.to(clientRoom).emit('twChatResend', msg);
         }
-        io.emit('twChatResend', msg);
     });
 
     socket.on('twchatReceiveMessages', (msg) => {
